7d6507f39b689a005096dd2edc6c6c75f82f460b
Koiti-Note <evandrokoiti@gmail.com>
2013-12-11 20:50:24 -0200

    fix torneios

--- lib/torneios.php
+++ lib/torneios.php
@@ -9,10 +9,12 @@
 	$torneios = $banco->ListaTorneiosCompleta();
 
     #data de hj
-    $data = date("d/m/Y");
+    $data1 = date("d/m/Y");
+    $data2 = date("d/m/Y", strtotime("+1 day"));
     
 	#Imprimi valores
 	$Conteudo = $banco->CarregaHtml('torneios');
-    $Conteudo = str_replace('<%DATA%>',$data,$Conteudo);
+    $Conteudo = str_replace('<%DATA1%>',$data1,$Conteudo);
+    $Conteudo = str_replace('<%DATA2%>',$data2,$Conteudo);
 	$Conteudo = str_replace('<%TORNEIOS%>',$torneios,$Conteudo);
 ?>
